e10dbde59d71e2d3905a4c97c782ac1813c18500
LAPTOP-V5TCK0NT\drag0 <drag0n_freak@hotmail.com>
2019-01-19 16:27:08 +0800

    no message

--- SoCheapskate/background.js
+++ SoCheapskate/background.js
@@ -12,14 +12,3 @@
       }]);
     });
   });
-
-  
-  
-  // A generic onclick callback function.
-function genericOnClick(info, tab) {
-  let url = "popup.html?selection="+ info.selectionText;
-  chrome.tabs.create({url:url, index: tab.index +1});
-}
-
-chrome.contextMenus.create({"title": "Search Using SoCheapskate", ["page","selection","link","editable","image","video",
-                "audio"],"onclick": genericOnClick});
---
+++ SoCheapskate/contextMenus.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+chrome.contextMenus.create({
+    id: "context",
+    title: "Search Using SoCheapskate",
+    contexts: ["all"]
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    let url = "popup.html?selection="+ info.selectionText;
+	chrome.tabs.create({url:url, index: tab.index +1});
+});

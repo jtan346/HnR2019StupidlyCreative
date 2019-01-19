5ca9c01d7effc9186db75e0a03075b6e43e24529
Zhemin Liu <zliu023@e.ntu.edu.sg>
2019-01-19 16:16:36 +0800

    Fix Context Menu to open new tab

--- SoCheapskate/background.js
+++ SoCheapskate/background.js
@@ -15,11 +15,15 @@
 
   
   
-  // A generic onclick callback function.
-function genericOnClick(info, tab) {
-  let url = "popup.html?selection="+ info.selectionText;
-  chrome.tabs.create({url:url, index: tab.index +1});
-}
+chrome.contextMenus.create({
+  id: "some-command",
+  title: "Search Using SoCheapskate",
+  contexts: ["page","selection","link","editable","image","video","audio"],
+});
 
-chrome.contextMenus.create({"title": "Search Using SoCheapskate", ["page","selection","link","editable","image","video",
-                "audio"],"onclick": genericOnClick});
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId == "some-command") {
+      let url = "popup.html?selection="+ info.selectionText;
+      chrome.tabs.create({url:url, index: tab.index +1});
+    }
+});
